a7977e32909ddd55fe018ca2ddab75be1d39841a
ladycath5bm <leidycath.bm@gmail.com>
2022-03-31 23:41:52 -0500

    feat: add event to product visited

---
+++ database/migrations/2022_04_01_043026_create_product_visits_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductVisitsTable extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('product_visits', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('post_id')->constrained();
+            $table->string('ip', 120);
+            $table->string('browser', 120);
+            $table->string('os', 120);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('product_visits');
+    }
+}
